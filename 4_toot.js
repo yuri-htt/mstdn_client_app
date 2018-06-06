6db4febe799d5310d7a65a6fc4593a3959e0b10e
Yuri <yuri.tayama@linksports.co.jp>
2018-06-07 00:54:02 +0900

    mstdn app

---
+++ 4_toot.js
@@ -0,0 +1,25 @@
+const Mastodon = require('mastodon-api')
+const fs = require('fs')
+const path = require('path')
+const instanceUri = 'https://pawoo.net'
+
+// ファイルから情報を読み込む
+const token = fs.readFileSync(path.join(__dirname, 'token.json'))
+
+// マストドンのAPIクライアントを作成
+const M = new Mastodon({
+  access_token: token,
+  timeout_ms: 60 * 1000,
+  api_url: instanceUri + '/api/v1/'
+})
+
+// 発言(Toot)する
+M.post('statuses',
+  {status: 'コマンドラインからToot! :cawoli_off_fuwafuwaheart:'},
+  (err, data, res) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    console.log(res)
+  })
